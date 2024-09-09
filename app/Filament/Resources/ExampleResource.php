1364c2b3d9a7c98e37de5abbd7f34e68e0bb0947
James Pickard <james.pickard@gmail.com>
2024-09-09 23:06:32 +0100

    Create Example resource, model

---
+++ app/Filament/Resources/ExampleResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExampleResource\Pages;
+use App\Filament\Resources\ExampleResource\RelationManagers;
+use App\Models\Example;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExampleResource extends Resource
+{
+    protected static ?string $model = Example::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExamples::route('/'),
+            'create' => Pages\CreateExample::route('/create'),
+            'edit' => Pages\EditExample::route('/{record}/edit'),
+        ];
+    }
+}
